a8850d7098c2961ec5acac948093300b10a96633
Lucas Nunes <55895056+luk3mn@users.noreply.github.com>
2021-07-21 20:51:58 -0300

    Adicionado as figuras quando os jogadores 1 e 2 clicarem em suas respectivas vezes

--- scripts/jogo.js
+++ scripts/jogo.js
@@ -1,77 +1,47 @@
+let jogador1=false;
+let jogador2=false;
+
 /******** CRINDO AS DIVS *********/
 function criaBlocos() {
   for (let i = 1; i <= 9; i++) {
     let bloco = document.createElement("div");
-    // let circle = document.createElement("img");
-    // let x = document.createElement("img");
-
-    // Atribui tags dentro da div
-    // bloco.appendChild(circle);
-    // bloco.appendChild(x);
-    // circle.src = "assets/circle.png"
-    // x.src = "assets/xis.png";
 
     // Ação ao clicar
-    // bloco.onclick = function() { showImage(this) }
-
-    // Seta atributos nas img
-    bloco.setAttribute("id", "block"+i);
-    // x.setAttribute("class","xis");
-    // circle.setAttribute("class","circle");
-
+    bloco.onclick = function() { inGaming(this) }
+    
+    bloco.id = 'block'+i
     document.querySelector('.tab').appendChild(bloco);
   }
 }
-criaBlocos();
 
-let jogador1=false;
-let jogador2=false;
 function player1() {
   document.querySelector('.panel-player .player').innerHTML="Jogador 1";
   jogador1=true;
+  jogador2=false;
 }
 
 function player2() {
   document.querySelector('.panel-player .player').innerHTML="Jogador 2";
+  jogador1=false;
   jogador2=true;
 }
 
 /* LOGIDA DO FUNCIONAMENTO DA RODADA */
-function inGaming() {
-  // Lógica de jogadas
-  for (let i=1; i <= 9; i++) {
-    let block = document.querySelector("#block"+i)
-    let circle = document.createElement("img");
-    let x = document.createElement("img");
-    
-    // Quando clicar em um bloco
-    document.querySelector('#block'+i).addEventListener("click", () => {
-      
-      if (jogador1) {
-        // block.appendChild(x);
-        // x.src = "assets/xis.png";
-        console.log("X")
-        jogador1=false;
-        player2()
-      }
+function inGaming(e) {
 
-      if (jogador2) {
-        // block.appendChild(circle);
-        // x.src = "assets/circle.png";
-        console.log("O");
-        jogador2=false;
-        player1()
-      }
-      
-      console.log("Jogador1: "+jogador1);
-      console.log("Jogador2: "+jogador2);
-    })
-  }
+  if (jogador1) {
+    e.innerHTML = "<img src='assets/xis.png'>";
+    player2()
+  } else {
+    e.innerHTML = "<img src='assets/circle.png'>";
+    player1()
+  } 
+  
 }
 
 /* QUANDO O BOTÃO DE 'COMEÇAR FOR CLICADO' */
 function startGame() {
-
+  
   let btnStart = document.querySelector('#btn-start');
   btnStart.addEventListener('click', () => {
     // Oculta o botão de começar a partida
@@ -82,14 +52,14 @@ function startGame() {
     player1() // Indica o jogador que vai começar o game
     document.querySelector('#player1').value=0
     document.querySelector('#player2').value=0
-    inGaming();
+    criaBlocos();
   })
-
+  
   /* DESISTIR DA PARTIDA */
   let btnLeft = document.querySelector('#btn-left');
   btnLeft.addEventListener('click', () => {
     let confirm = window.confirm("DESEJA ABANDONAR A PARTIDA?");
-
+    
     if (confirm) {
       if (jogador1) {
         alert("Jogador 1 PERDEU!!");
@@ -97,7 +67,7 @@ function startGame() {
       if (jogador2) {
         alert("JOGADOR 2 PERDEU!!");
       }
-
+      
       // Recarrega a página
       document.location.reload();
     } 
@@ -105,30 +75,11 @@ function startGame() {
 }
 
 
+// criaBlocos();
 startGame()
 
-// console.log(started)
-// function showImage() {
-
-//   /* CRIA OS ELEMENTOS DE IMG */
-//   let circle = document.createElement("img");
-//   // let x = document.createElement("img");
-
-//   /* ATRUBUI OS FILHOS */
-//   document.querySelector('#block1').appendChild(circle);
-//   // document.querySelector('#block1').appendChild(x);
-  
-//   /* COLOCA O CAMINHO DA IMG */
-//   circle.src = "assets/circle.png"
-//   // x.src = "assets/xis.png";
-
-//   document.querySelector('#block1').appendChild(bloco);
-  
-// }
-
-
 /**** LÓGICA:
- * Uma opção é add as imagens na div que for clicada
- * Outra opção é deixar todas as imagens ocultas na div do bloco e mostrar apenas quando clicar
+ * Cria a logica de identificar quando tres figuras iguais estiverem alinhadas
+ * Uma alternativa é criar um vetor o matriz e testar quando as posições combinarem
  * 
  ****/
